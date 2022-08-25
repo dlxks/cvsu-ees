53c157c4a2b9dbe22d2ebdf66c6649cda2a1e390
dlxks <dlxks.sangangbayan@gmail.com>
2022-08-25 08:42:28 +0800

    Update database connection

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://sjlbvvsmyudbtc:04ff1851a279bbc09890406274d2b6bb32df6abc02dffd4fbe4cc8791c95593e@ec2-44-209-186-51.compute-1.amazonaws.com:5432/ddb8v3l9ctv1p0');
+
 return [
 
     /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +67,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            // 'url' => env('DATABASE_URL'),
+            'host' => $DATABASE_URL['host'],
+            'port' =>  $DATABASE_URL['port'],
+            'database' => ltrim($ $DATABASE_URL['path'], '/'),
+            'username' =>  $DATABASE_URL['user'],
+            'password' =>  $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
