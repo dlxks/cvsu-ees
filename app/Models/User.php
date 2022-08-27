3ce327c40251179d52f20c28b76926ca0cec1dd0
dlxks <dlxks.sangangbayan@gmail.com>
2022-08-27 20:09:28 +0800

    User Commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'phone',
+        'phone_number',
         'role',
         'password',
     ];
