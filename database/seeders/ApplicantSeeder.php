8a8fc15094a24e0ad175318ac8212ff75d48d555
dlxks <dlxks.sangangbayan@gmail.com>
2022-08-27 20:53:42 +0800

    v2

--- database/seeders/ApplicantSeeder.php
+++ database/seeders/ApplicantSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Helpers\Helper;
 use App\Models\Applicant;
+use App\Models\User;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -21,27 +23,43 @@ class ApplicantSeeder extends Seeder
     {
         // factory(Applicant::class, 10)->create();
 
-        // $applicant_id = IdGenerator::generate(['table' => 'applicants', 'length' => 10, 'prefix' => date('ym'), 'reset_on_prefix_change' => true]);
 
-        DB::table('applicants')->insert([
-            [
-                'user_id' => '3',
-                'fname' => 'Tristan',
-                'mname' => 'Martinez',
-                'lname' => 'Sangangbayan',
-                'birthday' => '2000-08-31',
-                'email' => 'sangangbayant@gmail.com',
-                'phone_number' => '639072203266',
-            ],
-            [
-                'user_id' => '4',
-                'fname' => 'Dan Lloyd',
-                'mname' => '',
-                'lname' => 'Rosarda',
-                'birthday' => '1999-07-03',
-                'email' => 'danlloyd.rosarda@cvsu.edu.ph',
-                'phone_number' => '639761830787',
-            ],
+        $applicant_id = Helper::IDGenerator(new Applicant(), 'id', 5, date('ym'));
+
+        $user1 = User::create([
+            'name' =>  'Tristan Sangangbayan',
+            'email' => 'sangangbayant@gmail.com',
+            'phone_number' => '639072203266',
+            'role' => 'applicant',
+            'password' => bcrypt('changetorandomstring')
+        ]);
+        $applicant1 = Applicant::create([
+            'id' => $applicant_id,
+            'user_id' => $user1->id,
+            'fname' => 'Tristan',
+            'mname' => 'Martinez',
+            'lname' => 'Sangangbayan',
+            'birthday' => '2000-08-31',
+            'email' => 'sangangbayant@gmail.com',
+            'phone_number' => '639072203266',
+        ]);
+
+        $user2 = User::create([
+            'name' =>  'Dan Lloyd Rosarda',
+            'email' => 'ljohnmark9@gmail.com',
+            'phone_number' => '639558076388',
+            'role' => 'applicant',
+            'password' => bcrypt('changetorandomstring')
+        ]);
+        $applicant2 = Applicant::create([
+            'id' => $applicant_id,
+            'user_id' => $user2->id,
+            'fname' => 'Dan Lloyd',
+            'mname' => 'Martinez',
+            'lname' => 'Rosarda',
+            'birthday' => '2000-07-31',
+            'email' => 'ljohnmark9@gmail.com',
+            'phone_number' => '639558076388',
         ]);
     }
 }
